dd4f16a962e34f024f2ec9b2117c2c8362790a9a
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2022-07-19 12:54:31 -0400

    - added download on injection server - attempt to fix the disk usage issue

--- extractServer/server.go
+++ extractServer/server.go
@@ -3,20 +3,42 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-msgio"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"path"
 )
 
 const (
-	HOST = "0.0.0.0"
-	PORT = "9999"
-	TYPE = "tcp"
+	HOST    = "0.0.0.0"
+	PORT    = "9999"
+	TYPE    = "tcp"
+	SaveDir = "/out/"
 )
 
-type tcpServer struct {
-	writer msgio.Writer
+func downloadFile(cid cid.Cid, saveDir string, gatewayUrl string) {
+	log.Printf("Downloading cid %s", cid)
+	// files that might be keys
+	fileData, err := http.Get(fmt.Sprintf("%s/ipfs/%s", gatewayUrl, cid))
+	if err != nil {
+		log.Printf("Failed download cid %s", cid)
+	}
+	saveFile := path.Join(saveDir, cid.String())
+	out, err := os.Create(saveFile)
+	if err != nil {
+		log.Printf("Failed create cid file %s", cid)
+	}
+
+	// Write the body to file
+	_, err = io.Copy(out, fileData.Body)
+	if err != nil {
+		log.Printf("Failed create cid file %s", cid)
+	}
+	out.Close()
 }
 
 func main() {
@@ -25,6 +47,13 @@ func main() {
 	if !ok {
 		url = HOST + ":" + PORT
 	}
+
+	gatewayUrl, ok := os.LookupEnv("IPFS_GATEWAY_URL")
+	if !ok {
+		url = "http://127.0.0.1:8080"
+	}
+	log.Printf("Gateway addrs %s", gatewayUrl)
+
 	listen, err := net.Listen(TYPE, url)
 	if err != nil {
 		log.Fatal(err)
@@ -39,10 +68,11 @@ func main() {
 			log.Fatal(err)
 			os.Exit(1)
 		}
-		go handleIncomingRequest(conn)
+		go handleIncomingRequest(conn, gatewayUrl)
 	}
 }
-func handleIncomingRequest(c net.Conn) {
+
+func handleIncomingRequest(c net.Conn, gatewayUrl string) {
 	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
 	reader := msgio.NewReader(c)
 	for {
@@ -57,8 +87,14 @@ func handleIncomingRequest(c net.Conn) {
 			log.Printf("Failed unmarshal data %s", msg)
 			continue
 		}
-		log.Printf(msgRecvd.Cid)
-
+		log.Printf("Processing %s with type of %s", msgRecvd.Cid, msgRecvd.FileType)
+		newCid, err := cid.Decode(msgRecvd.Cid)
+		if err != nil {
+			log.Printf("Invalid cid %s", msgRecvd.Cid)
+			continue
+		}
+		// download cid
+		downloadFile(newCid, SaveDir, gatewayUrl)
 	}
 	c.Close()
 }
